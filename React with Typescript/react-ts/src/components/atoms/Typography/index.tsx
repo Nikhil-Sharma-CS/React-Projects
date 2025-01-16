68682d377751958b262fde82e0073ae5422454c8
Nikhil Sharma <nikhil.sharma@zemosolabs.com>
2025-01-16 22:07:07 +0530

    :tada: feat: started with react ts with pocket-pay figma login screen

---
+++ React with Typescript/react-ts/src/components/atoms/Typography/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  Typography as MuiTypography,
+  TypographyProps as MuiTypographyProps,
+} from "@mui/material";
+
+interface TypographyProps extends MuiTypographyProps {
+  text: string;
+  variant: "h1" | "h2" | "h3" | "h4" | "h5";
+}
+
+const Typography: React.FC<TypographyProps> = ({ ...props }) => {
+  return <MuiTypography variant={props.variant}>{props.text}</MuiTypography>;
+};
+
+export default Typography;
---
+++ React with Typescript/react-ts/src/components/utils/constants/index.ts
@@ -0,0 +1,10 @@
+export const TextConstants = {
+  ENTER_YOUR_PASSWORD: "Enter your password",
+  ENTER_YOUR_EMAIl: "Enter your email address",
+  WELCOME_BACK: "Welcome Back",
+  LOG_IN: "Log in",
+  REMEMBER_ME: "Remember me",
+  TROUBLE_LOGGIN_IN: "Trouble logging in?",
+  OR_LOGIN_WITH: "Or, Log in with",
+  POCKET_PAY: "Pocket Pay",
+};
