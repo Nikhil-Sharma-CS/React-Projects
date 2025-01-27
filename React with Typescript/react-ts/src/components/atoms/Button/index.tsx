0c9337f3d9f8910ea1280c33389c9d4b216dadc5
Nikhil-Sharma-CS <nikhilsharma9076@gmail.com>
2025-01-27 21:23:01 +0530

    :fire: fix: seperated styled button from index

--- React with Typescript/react-ts/src/components/atoms/Button/index.tsx
+++ React with Typescript/react-ts/src/components/atoms/Button/index.tsx
@@ -1,22 +1,12 @@
 import React from "react";
-import {
-  Button as MuiButton,
-  ButtonProps as MuiButtonProps,
-  styled,
-} from "@mui/material";
-import { theme } from "../../../theme";
+import { ButtonProps as MuiButtonProps } from "@mui/material";
+import { StyledButton } from "./index.styles";
 
 interface ButtonProps extends MuiButtonProps {
   text: string;
   onClick?: () => void;
 }
 
-const StyledButton = styled(MuiButton)(({}) => ({
-  width: "516px",
-  backgroundColor: theme.palette.secondary.light,
-  borderRadius: "56px",
-  boxShadow: "#5533FF3D",
-}));
 const Button: React.FC<ButtonProps> = ({ ...props }) => {
   return <StyledButton onClick={props.onClick}>{props.text}</StyledButton>;
 };
