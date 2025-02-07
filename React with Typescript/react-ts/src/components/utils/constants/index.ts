10b6d8938ffe54f85f43d9e956bff3f3daf0f7da
Nikhil-Sharma-CS <nikhilsharma9076@gmail.com>
2025-02-07 23:46:41 +0530

    :construction: feat: modified icons constants with icon details

--- React with Typescript/react-ts/src/components/utils/constants/index.ts
+++ React with Typescript/react-ts/src/components/utils/constants/index.ts
@@ -1,3 +1,7 @@
+import GOOGLEICON from "../../../../public/assets/icons/google.svg";
+import FACEBOOKICON from "../../../../public/assets/icons/facebook.svg";
+import APPLEICON from "../../../../public/assets/icons/apple.svg";
+
 export const TextConstants = {
   ENTER_YOUR_PASSWORD: "Enter your password",
   ENTER_YOUR_EMAIl: "Enter your email address",
@@ -8,6 +12,16 @@ export const TextConstants = {
   OR_LOGIN_WITH: "Or, Log in with",
   POCKET_PAY: "Pocket Pay",
   SIGN_IN: "Sign In",
+  GOOGLE_ICON: "Google Icon",
+  FACEBOOK_ICON: "Facebook Icon",
+  APPLE_ICON: "Apple Icon",
 };
 
-export const Icons = [{}, {}];
+export const Icons = [
+  {
+    src: GOOGLEICON,
+    alt: TextConstants.GOOGLE_ICON,
+  },
+  { src: FACEBOOKICON, alt: TextConstants.FACEBOOK_ICON },
+  { src: APPLEICON, alt: TextConstants.APPLE_ICON },
+];
