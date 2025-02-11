c411d9727a17a5b2d89363d6fbc078637d3abc91
Nikhil-Sharma-CS <nikhilsharma9076@gmail.com>
2025-02-11 21:34:04 +0530

    :construction: wip: wip

--- React with Typescript/react-ts/src/components/molecules/IconList/index.tsx
+++ React with Typescript/react-ts/src/components/molecules/IconList/index.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Icons } from "../../utils/constants";
 import Icon from "../../atoms/Icon";
+import { Box, styled } from "@mui/material";
+
+const StyledIconContainer = styled(Box)(({}) => ({
+  display: "flex",
+}));
 
 const IconList = () => {
   return (
     <>
-      {Icons.map((icon, index) => (
-        <Icon
-          key={index}
-          src={icon.src}
-          alt={icon.alt}
-          width={40}
-          height={40}
-        />
-      ))}
+      <StyledIconContainer>
+        {Icons.map((icon, index) => (
+          <Icon
+            key={index}
+            src={icon.src}
+            alt={icon.alt}
+            width={40}
+            height={40}
+          />
+        ))}
+      </StyledIconContainer>
     </>
   );
 };
